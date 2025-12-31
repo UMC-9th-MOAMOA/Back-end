eb8d317641152e1623fc3cb364e3d6299384aa1e
minjaehan <tommy3942@naver.com>
2025-12-31 19:13:51 +0900

    settings : 스프링부트 프로젝트 생성

---
+++ src/main/java/com/example/moamoa_backend/MoamoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moamoa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoamoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoamoaBackendApplication.class, args);
+    }
+
+}
