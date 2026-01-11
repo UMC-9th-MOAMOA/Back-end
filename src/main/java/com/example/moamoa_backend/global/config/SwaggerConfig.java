28e6b32cf1c61f92ef3dc1dc1dd1309b43a1618e
YoungJJun <young_879@naver.com>
2026-01-11 15:21:06 +0900

    feat: 회원가입 구현을 위한 초기 설정파일 구현
    - SecurityConfig: BCryptPasswordEncoder 빈 등록 및 기본 보안 필터 구성 (CSRF 비활성화, 인증 경로 허용)
    - RedisConfig: RedisTemplate 설정 및 LettuceConnectionFactory 연결
    - SwaggerConfig: API 문서화 설정 및 JWT 인증(Authorize) 버튼 활성화

---
+++ src/main/java/com/example/moamoa_backend/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.example.moamoa_backend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swagger() {
+        Info info = new Info()
+                .title("MOAMOA API")
+                .description("UMC 9기 MOAMOA 프로젝트 API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
