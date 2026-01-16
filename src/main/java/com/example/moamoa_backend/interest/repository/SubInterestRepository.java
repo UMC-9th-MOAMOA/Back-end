0fb37473dc358932f9be7cf9005b4b083cb096c9
zeroColaa <sinyoung556@gmail.com>
2026-01-17 00:07:02 +0900

    feat: 세부 관심사 검증 로직 및 온보딩 에러코드 추가

--- src/main/java/com/example/moamoa_backend/interest/repository/SubInterestRepository.java
+++ src/main/java/com/example/moamoa_backend/interest/repository/SubInterestRepository.java
@@ -2,11 +2,24 @@ package com.example.moamoa_backend.interest.repository;
 
 import com.example.moamoa_backend.interest.entity.SubInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface SubInterestRepository extends JpaRepository<SubInterest, Long> {
 
-	// interest_id로 세부관심사 전체 조회
 	List<SubInterest> findAllByInterest_IdOrderByIdAsc(Long interestId);
+
+	interface InterestSubPair {
+		Long getInterestId();
+		Long getSubInterestId();
+	}
+
+	@Query("""
+        select si.interest.id as interestId, si.id as subInterestId
+        from SubInterest si
+        where si.id in :subIds
+    """)
+	List<InterestSubPair> findInterestSubPairsBySubIds(Collection<Long> subIds);
 }
