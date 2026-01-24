f4974d77053f9573b6cd0ad04ac275a372b82521
zeroColaa <sinyoung556@gmail.com>
2026-01-25 00:07:03 +0900

    refactor: InterestSuccessCode 포맷 수정

--- src/main/java/com/example/moamoa_backend/interest/exception/code/InterestSuccessCode.java
+++ src/main/java/com/example/moamoa_backend/interest/exception/code/InterestSuccessCode.java
@@ -1,16 +1,27 @@
 package com.example.moamoa_backend.interest.exception.code;
 
 import com.example.moamoa_backend.global.apiPayload.code.BaseSuccessCode;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum InterestSuccessCode implements BaseSuccessCode {
 
-	INTEREST_LIST_OK(HttpStatus.OK, "INTEREST200_1", "관심사 목록 조회에 성공했습니다."),
-	SUB_INTEREST_LIST_OK(HttpStatus.OK, "INTEREST200_2", "세부 관심사 목록 조회에 성공했습니다.");
+	INTEREST_LIST_OK(
+		HttpStatus.OK,
+		"INTEREST200_1",
+		"관심사 목록 조회에 성공했습니다."
+	),
+
+	SUB_INTEREST_LIST_OK(
+		HttpStatus.OK,
+		"INTEREST200_2",
+		"세부 관심사 목록 조회에 성공했습니다."
+	);
 
 	private final HttpStatus status;
 	private final String code;
