72fe9642b18808d66d83fa82d0c0b75ecf2a7342
YoungJJun <young_879@naver.com>
2026-01-27 21:52:03 +0900

    fix: MemberPolicy.updateAgreement() if문 제거 누락 수정

--- src/main/java/com/example/moamoa_backend/policy/entity/MemberPolicy.java
+++ src/main/java/com/example/moamoa_backend/policy/entity/MemberPolicy.java
@@ -40,8 +40,6 @@ public class MemberPolicy extends BaseEntity {
 
     public void updateAgreement(boolean isAgreed) {
         this.isAgreed = isAgreed;
-        if (isAgreed) {
-            this.agreedAt = isAgreed ? LocalDateTime.now() : null;
-        }
+        this.agreedAt = isAgreed ? LocalDateTime.now() : null;
     }
 }
