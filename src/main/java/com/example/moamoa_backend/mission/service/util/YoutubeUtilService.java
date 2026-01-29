7688ced4362b121e16a46a2bb2e0c61cd1bc70d0
haewonee <godnjs5870@naver.com>
2026-01-30 03:57:46 +0900

    fix: 정규식 수정

--- src/main/java/com/example/moamoa_backend/mission/service/util/YoutubeUtilService.java
+++ src/main/java/com/example/moamoa_backend/mission/service/util/YoutubeUtilService.java
@@ -56,7 +56,7 @@ public class YoutubeUtilService {
         }
     }
     private String extractVideoId(String url) {
-        String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
+        String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
         Pattern compiledPattern = Pattern.compile(pattern);
         Matcher matcher = compiledPattern.matcher(url);
         return matcher.find() ? matcher.group() : null;
