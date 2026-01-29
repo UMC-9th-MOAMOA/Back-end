7a29e455629d4f1b0d73fc4ff5bb6a06de515ac6
haewonee <godnjs5870@naver.com>
2026-01-30 00:19:56 +0900

    chore: 에러코드 수정

--- src/main/java/com/example/moamoa_backend/mission/exception/code/MissionErrorCode.java
+++ src/main/java/com/example/moamoa_backend/mission/exception/code/MissionErrorCode.java
@@ -15,7 +15,7 @@ public enum MissionErrorCode implements BaseErrorCode {
     QUIZ_JSON_CONVERSION_FAIL(HttpStatus.BAD_REQUEST, "MISSION400_1", "퀴즈 데이터 JSON 변환 실패. 형식을 맞춰주세요."),
     INVALID_MISSION_STATUS(HttpStatus.BAD_REQUEST, "MISSION400_2", "잘못된 미션 상태 변경 요청입니다."),
     MISSION_NOT_STARTED(HttpStatus.BAD_REQUEST, "MISSION400_3", "아직 미션을 시작하지 않았습니다. 도전하기를 먼저 눌러주세요."),
-    INVALID_MISSION_STATUS_PARAM(HttpStatus.BAD_REQUEST, "MISSION400_1", "유효하지 않은 미션 상태(status) 파라미터입니다.");
+    INVALID_MISSION_STATUS_PARAM(HttpStatus.BAD_REQUEST, "MISSION400_4", "유효하지 않은 미션 상태(status) 파라미터입니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
