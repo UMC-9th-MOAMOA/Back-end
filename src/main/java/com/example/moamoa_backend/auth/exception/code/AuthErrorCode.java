87d7340a1ea96de38a20a4dfed33e743aa627880
YoungJJun <young_879@naver.com>
2026-02-02 01:36:19 +0900

    refactor: AuthErrorCode 미사용 주석 제거

--- src/main/java/com/example/moamoa_backend/auth/exception/code/AuthErrorCode.java
+++ src/main/java/com/example/moamoa_backend/auth/exception/code/AuthErrorCode.java
@@ -94,7 +94,6 @@ public enum AuthErrorCode implements BaseErrorCode {
             "AUTH403_4",
             "활성화되지 않은 계정입니다. 고객센터에 문의해주세요."),
 
-    // AuthErrorCode 또는 MemberErrorCode에 추가
     POLICY_NOT_AGREED(HttpStatus.FORBIDDEN,
             "AUTH403_5",
             "아직 정책에 동의하지 않은 회원입니다. 정책 동의 후 이용해주세요."),
