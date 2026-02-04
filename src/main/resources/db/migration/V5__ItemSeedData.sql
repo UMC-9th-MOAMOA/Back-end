c18cd4c3aa059248686aa66a15a8cf881db6d33d
minjaehan <tommy3942@naver.com>
2026-02-04 23:35:02 +0900

    chore: [Flyway] 아이템 데이터 삽입

---
+++ src/main/resources/db/migration/V5__ItemSeedData.sql
@@ -0,0 +1,36 @@
+INSERT INTO item (id, created_at, updated_at, price, type, name, image_url, is_on_sale)  VALUES
+(1,NOW(),NOW(), 60,'FACE','표정1','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/face/%ED%91%9C%EC%A0%951.svg',true),
+(2,NOW(),NOW(), 60,'FACE','표정2','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/face/%ED%91%9C%EC%A0%952.svg',true),
+(3,NOW(),NOW(), 50,'FACE','표정3','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/face/%ED%91%9C%EC%A0%953.svg',true),
+(4,NOW(),NOW(), 40,'FACE','표정4','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/face/%ED%91%9C%EC%A0%954.svg',true),
+(5,NOW(),NOW(), 50,'FACE','표정5','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/face/%ED%91%9C%EC%A0%955.svg',true),
+(6,NOW(),NOW(), 40,'FACE','표정6','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/face/%ED%91%9C%EC%A0%956.svg',true),
+
+(11,NOW(),NOW(), 530,'TOP','상의1','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/top/%EC%83%81%EC%9D%981.svg',true),
+(12,NOW(),NOW(), 550,'TOP','상의2','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/top/%EC%83%81%EC%9D%982.svg',true),
+(13,NOW(),NOW(), 450,'TOP','상의3','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/top/%EC%83%81%EC%9D%983.svg',true),
+(14,NOW(),NOW(), 480,'TOP','상의4','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/top/%EC%83%81%EC%9D%984.svg',true),
+(15,NOW(),NOW(), 600,'TOP','상의5','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/top/%EC%83%81%EC%9D%985.svg',true),
+(16,NOW(),NOW(), 470,'TOP','상의6','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/top/%EC%83%81%EC%9D%986.svg',true),
+
+(21,NOW(),NOW(), 450,'BOTTOM','하의1','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%981.svg',true),
+(22,NOW(),NOW(), 390,'BOTTOM','하의2','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%982.svg',true),
+(23,NOW(),NOW(), 430,'BOTTOM','하의3','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%983.svg',true),
+(24,NOW(),NOW(), 370,'BOTTOM','하의4','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%984.svg',true),
+(25,NOW(),NOW(), 410,'BOTTOM','하의5','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%985.svg',true),
+(26,NOW(),NOW(), 430,'BOTTOM','하의6','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%986.svg',true),
+
+(31,NOW(),NOW(), 180,'HAT','모자','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/accessory/%EB%AA%A8%EC%9E%90.svg',true),
+(32,NOW(),NOW(), 200,'GLASSES','안경','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/accessory/%EC%95%88%EA%B2%BD.svg',true),
+(33,NOW(),NOW(), 160,'GLOVES','장갑','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/accessory/%EC%9E%A5%EA%B0%91.svg',true),
+(34,NOW(),NOW(), 200,'SHOES','신발','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/accessory/%EC%8B%A0%EB%B0%9C.svg',true),
+(35,NOW(),NOW(), 150,'HAT','귀도리','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/accessory/%EA%B7%80%EB%8F%84%EB%A6%AC.svg',true),
+(36,NOW(),NOW(), 170,'SCARF','목도리','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/accessory/%EB%AA%A9%EB%8F%84%EB%A6%AC.svg',true),
+
+(41,NOW(),NOW(), 700,'BACKGROUND','방안1','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/background/%EB%B0%A9%EC%95%881.svg',true),
+(42,NOW(),NOW(), 750,'BACKGROUND','방안2','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/background/%EB%B0%A9%EC%95%882.svg',true),
+(43,NOW(),NOW(), 800,'BACKGROUND','방안3','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/background/%EB%B0%A9%EC%95%883.svg',true),
+(44,NOW(),NOW(), 850,'BACKGROUND','들판','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/background/%EB%93%A4%ED%8C%90.svg',true),
+(45,NOW(),NOW(), 900,'BACKGROUND','언덕','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/background/%EC%96%B8%EB%8D%95.svg',true),
+(46,NOW(),NOW(), 1000,'BACKGROUND','바다','https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/background/%EB%B0%94%EB%8B%A4.svg',true)
+
