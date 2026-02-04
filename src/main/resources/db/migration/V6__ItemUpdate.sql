18c73b0acde3905ae21dac18c4655b711e81cdb6
minjaehan <tommy3942@naver.com>
2026-02-05 01:32:48 +0900

    chore: [Flyway] 아이템(하의 2,4,5) 데이터 업데이트

---
+++ src/main/resources/db/migration/V6__ItemUpdate.sql
@@ -0,0 +1,11 @@
+UPDATE item
+SET image_url = 'https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%982-1.svg'
+WHERE id = 22;
+
+UPDATE item
+SET  image_url='https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%984-1.svg'
+WHERE id=24;
+
+UPDATE item
+SET image_url='https://moamoa-s3-prod-bucket.s3.ap-northeast-2.amazonaws.com/public/items/bottom/%ED%95%98%EC%9D%985-1.svg'
+WHERE id=25;
